41d5e03c8a1ace23b563ddca4ae577aeda9cab08
Zihao1319 <zihao1319@gmail.com>
2023-03-03 09:13:08 +0800

    first commit

---
+++ src/main/java/batch2_test/batch2/test/Batch2TestApplication.java
@@ -0,0 +1,13 @@
+package batch2_test.batch2.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch2TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch2TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/batch2_test/batch2/test/Batch2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package batch2_test.batch2.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch2TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
